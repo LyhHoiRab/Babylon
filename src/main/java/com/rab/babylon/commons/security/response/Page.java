a927163c352c456de32f9142fa331159bc52d197
lihaihua <lihaihua@email.com>
2017-10-16 17:43:37 +0800

    增加分页工具是否有下一页属性

--- src/main/java/com/rab/babylon/commons/security/response/Page.java
+++ src/main/java/com/rab/babylon/commons/security/response/Page.java
@@ -14,11 +14,14 @@ public class Page<T>{
     private PageRequest pageRequest;
     //分页数据
     private List<T> content = new ArrayList<T>();
+    //是否有下一页
+    private Boolean hasNext;
 
     public Page(List<T> content, PageRequest pageRequest, Long total){
         this.pageRequest = pageRequest;
         this.content.addAll(content);
         this.total = (!content.isEmpty() && pageRequest != null && (pageRequest.getOffset() + pageRequest.getPageSize()) > total) ? pageRequest.getOffset() + content.size() : total;
+        this.hasNext = (pageRequest.getOffset() + content.size() < this.total);
     }
 
     public Long getTotal(){
@@ -43,5 +46,13 @@ public class Page<T>{
 
     public void setContent(List<T> content){
         this.content = content;
+    }
+
+    public Boolean getHasNext(){
+        return hasNext;
+    }
+
+    public void setHasNext(Boolean hasNext){
+        this.hasNext = hasNext;
     }
 }
